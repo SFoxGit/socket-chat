4b935b02362cbc71d0832bbe45895930fe6ca4d1
Shawn Fox <foxeasttrojan06@gmail.com>
2021-05-04 16:12:29 -0400

    sending messages

--- client/src/contexts/ConversationsProvider.js
+++ client/src/contexts/ConversationsProvider.js
@@ -1,22 +1,58 @@
-import React, {useContext, useState} from 'react';
+import React, { useContext, useState } from 'react';
 import useLocalStorage from '../utils/useLocalStorage';
 import { useContacts } from './ContactsProvider';
 
 const ConversationsContext = React.createContext()
 
+function arrayEquality(a, b) {
+  if (a.length !== b.length) return false
+  a.sort()
+  b.sort()
+  return a.every((element, index) => {
+    return element === b[index]
+  })
+}
+
 export function useConversations() {
   return useContext(ConversationsContext)
 }
 
-export function ConversationsProvider({children}) {
+export function ConversationsProvider({ id, children }) {
   const [conversations, setConversations] = useLocalStorage('Conversations', []);
   const { contacts } = useContacts();
   const [selectedConversationIndex, setSelectedConversationIndex] = useState(0)
 
   function createConversation(recipients) {
     setConversations(prevConversations => {
-      return [...prevConversations, {recipients, messages: []}]
+      return [...prevConversations, { recipients, messages: [] }]
     })
+  }
+
+  function addMessageToConversation({ recipients, text, sender }) {
+    setConversations(prevConversations => {
+      let madeChange = false
+      const newMessage = { sender, text }
+      const newConversations = prevConversations.map(conversation => {
+        if (arrayEquality(conversation.recipients, recipients)) {
+          madeChange = true
+          return {
+            ...conversation,
+            messages: [...conversation.messages, newMessage]
+          }
+        }
+        return conversation
+      })
+
+      if (madeChange) {
+        return newConversations
+      } else {
+        return [...prevConversations, { recipients, messages: [newMessage] }]
+      }
+    })
+  }
+
+  function sendMessage(recipients, text) {
+    addMessageToConversation({ recipients, text, sender: id })
   }
 
   const formattedConversations = conversations.map((conversation, index) => {
@@ -25,15 +61,16 @@ export function ConversationsProvider({children}) {
         return contact.id === recipient
       })
       const name = (contact && contact.name) || recipient
-      return { id: recipient, name}
+      return { id: recipient, name }
     })
     const selected = index === selectedConversationIndex
-    return {...conversations, recipients, selected}
+    return { ...conversations, recipients, selected }
   })
 
   const value = {
     conversations: formattedConversations,
     selectedConversation: formattedConversations[selectedConversationIndex],
+    sendMessage,
     selectedConversationIndex: setSelectedConversationIndex,
     createConversation
   }
