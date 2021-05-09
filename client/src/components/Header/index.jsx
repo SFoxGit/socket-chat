6516e82feb33819696e46f58c5642c606be27092
Shawn Fox <foxeasttrojan06@gmail.com>
2021-05-08 21:37:10 -0400

    header component

---
+++ client/src/components/Header/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Navbar, Nav } from 'react-bootstrap'
+
+function Header() {
+
+  return (
+    <Navbar collapseOnSelect expand="lg" variant="dark" sticky="top">
+      <Navbar.Brand className="brand" href="/">SocketChat</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+          <Nav.Link href="/login">Logout</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default Header;
